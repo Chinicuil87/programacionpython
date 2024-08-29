b87252264337ff50f68e7e5b76a17f3dd76a42e4
Cesar Lopez Orihuela <132300747+Chinicuil87@users.noreply.github.com>
2024-08-29 10:19:54 -0600

    Update main.py

--- proyecto1/main.py
+++ proyecto1/main.py
@@ -5,7 +5,7 @@ separador = "*"*50
 print(separador)
 print(" Bienvenido al generador de nombre de tu marca.")
 print(separador)
-nombre_animal = input("Decir em el nombre de un animal: ")
+nombre_animal = input("Decirme el nombre de un animal: ")
 color = input("Decirme un color:  ")
 print(separador)
 print("El nombre de tu marca es: " + nombre_animal + " " + color)
